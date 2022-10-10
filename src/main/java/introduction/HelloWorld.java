ff4d0fa5f7f464a2fa5768d1c8c7b5780d412efc
Miłosz Karpowicz <milosz.karpowicz@gmail.com>
2022-10-10 16:46:59 +0200

    zajecia 1

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,13 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
+    static Logger log = Logger.getLogger("abc");
+
     public static void main(String [ ] args) {
+        log.info("test");
         System.out.println("Hello! Witaj!");
     }
 }
